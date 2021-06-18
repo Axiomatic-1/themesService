198601ccd54a84be783c86e9d70b6092b6c5238c
axiomatic-1 <QPis29BRt@>
2021-06-18 21:27:06 +0300

    refactor

--- themes-services/src/test/java/com/microsevriceslesson/plateService/PlateServicesApplicationTests.java
+++ themes-services/src/test/java/com/microsevriceslesson/plateService/PlateServicesApplicationTests.java
@@ -1,4 +1,4 @@
-package com.microsevriceslesson.departmentservices;
+package com.microsevriceslesson.plateService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
