0186539299279cae92939ef9971c2c253ca0e074
axiomatic-1 <QPis29BRt@>
2021-06-18 18:24:27 +0300

    starter

---
+++ themes-services/src/main/java/com/microsevriceslesson/departmentservices/entity/Department.java
@@ -0,0 +1,27 @@
+package com.microsevriceslesson.departmentservices.entity;
+
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "departments")
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "department_id")
+    private Long departmentId;
+    @Column(name = "department_name")
+    private String departmentName;
+    @Column(name = "adress")
+    private String adress;
+    @Column(name = "department_code")
+    private String departmentCode;
+
+}
